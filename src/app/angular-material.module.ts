063411ab11e1f8be7f5786e0ae718d9259f7ce57
kine1991 <kine4599@gmail.com>
2019-10-11 15:15:15 +0300

    Initial commit

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+@NgModule({
+    imports: [],
+    exports: [
+        MatSliderModule,
+        MatGridListModule,
+    ],
+    providers: [],
+})
+export class AngularMaterialModule { }
