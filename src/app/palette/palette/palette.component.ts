20c8efc47fe292f49b1e3a20f76976361869560a
kine1991 <kine4599@gmail.com>
2019-11-30 13:12:57 +0300

    added method getPervAndNextPalette

--- src/app/palette/palette/palette.component.ts
+++ src/app/palette/palette/palette.component.ts
@@ -15,6 +15,9 @@ export class PaletteComponent implements OnInit {
   level = 700;
   format = 'hex';
 
+  prev = undefined;
+  next = undefined;
+
   constructor(
     private paletteService: PaletteService,
     private route: ActivatedRoute,
@@ -26,11 +29,36 @@ export class PaletteComponent implements OnInit {
     return this.paletteService.getPaletteByLevel(paletteId)
   }
 
+  getPervAndNextPalette(paletteId){
+    let allPaletteId = this.paletteService.seedColors
+    let arrayPaletteId = [];
+    allPaletteId.map(paletteId => {
+      arrayPaletteId.push(paletteId.id);
+    })
+
+    let index = arrayPaletteId.findIndex(item => item === paletteId);
+    // console.log('arrayPaletteId[index]', arrayPaletteId[index+1]);
+
+    if(arrayPaletteId[index + 1]){
+      this.next = arrayPaletteId[index + 1]
+    }else {
+      this.next = arrayPaletteId[0]
+    }
+    if(arrayPaletteId[index - 1]){
+      this.prev = arrayPaletteId[index - 1]
+    }else {
+      this.prev = arrayPaletteId[arrayPaletteId.length - 1]
+    }
+
+    // return this.paletteService.seedColors
+  }
+
   ngOnInit() {
     this.seedColors = this.paletteService.seedColors;
     this.route.paramMap.subscribe((params: Params) => {
       let paletteId = params.params.paletteId;
       this.palette = this.getPaletteByLevel(paletteId);
+      this.getPervAndNextPalette(paletteId)
     });
   }
 
